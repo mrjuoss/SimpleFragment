9124c3954255f79b6def19d17eefc75f28b5f8ba
mrjuoss <mrjuoss@gmail.com>
2019-06-24 18:29:50 +0700

    Finish simple fragment

--- app/src/main/java/com/mrjuoss/dicoding/simplefragment/MainActivity.java
+++ app/src/main/java/com/mrjuoss/dicoding/simplefragment/MainActivity.java
@@ -1,14 +1,77 @@
 package com.mrjuoss.dicoding.simplefragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+
+    Button btnHome, btnAbout, btnHelp, btnExit;
+    HomeFragment homeFragment;
+    AboutFragment aboutFragment;
+    HelpFragment helpFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHome = findViewById(R.id.btn_home);
+        btnAbout = findViewById(R.id.btn_about);
+        btnHelp = findViewById(R.id.btn_help);
+        btnExit = findViewById(R.id.btn_exit);
+
+        btnHome.setOnClickListener(this);
+        btnAbout.setOnClickListener(this);
+        btnHelp.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnHome) {
+            mHome();
+        }
+        if (view == btnAbout) {
+            mAbout();
+        }
+        if (view == btnHelp) {
+            mHelp();
+        }
+        if (view == btnExit) {
+            mExit();
+        }
+    }
+
+    private void mHome() {
+        homeFragment = new HomeFragment();
+        FragmentTransaction ft = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, homeFragment);
+        ft.commit();
+    }
+
+    private void mAbout() {
+        aboutFragment = new AboutFragment();
+        FragmentTransaction ft = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, aboutFragment);
+        ft.commit();
+    }
+
+    private void mHelp() {
+        helpFragment = new HelpFragment();
+        FragmentTransaction ft = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, helpFragment);
+        ft.commit();
+    }
+
+    private void mExit() {
+        finish();
     }
 }
